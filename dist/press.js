06885c5280e7f6f1d876d12453a4434856e0076f
tomasmrazek92 <tomasmrazek92@gmail.com>
2024-04-10 19:21:23 +0200

    BC - Update
    Moved Animation to reusable file.

--- dist/press.js
+++ dist/press.js
@@ -1,6 +1,1 @@
-"use strict";
-(() => {
-  // bin/live-reload.js
-  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
-})();
-//# sourceMappingURL=press.js.map
+"use strict";(()=>{})();
